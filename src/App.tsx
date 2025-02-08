d81f4968affd4d27a8788ba7e7da7a4d8b9f4c14
Kunal Sharma <159030119+kunalsharma-iitjmu@users.noreply.github.com>
2025-02-08 14:05:46 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ const App = () => {
     <Router>
       <Header />
       <Routes>
+        <Route path="/" element={<Home/>}></Route>
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path='/project' element={<Project />}></Route>
